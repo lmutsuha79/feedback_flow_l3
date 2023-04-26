ee7cf63a5b533d86eb2d62732c63fb1f692fae05
Khelil Yasser Djamel Eddie <74467521+lamani79@users.noreply.github.com>
2023-04-26 17:37:14 +0100

    add loading screen to dashboard

--- components/layout/dashboard-layout.js
+++ components/layout/dashboard-layout.js
@@ -4,15 +4,19 @@ import SelectAppDropDown from "./layout-components/select-app-drop";
 import TopNav from "./layout-components/top-nav";
 import { useUser } from "@supabase/auth-helpers-react";
 import LoadingScreen from "../ui/loading-screen";
+import { useState } from "react";
 
 const DashboardLayout = (props) => {
+  const [isLoading, setIsLoading] = useState(true);
   const user = useUser();
 
   if (user) {
     return (
       <>
-        <LoadingScreen />
-        <div className="container pt-4">
+        {isLoading && <LoadingScreen />}
+        <div
+          className={isLoading ? "blur-sm container pt-4" : "container pt-4"}
+        >
           <div className="border border-slate-200 rounded-md">
             <header className="h-[64px]">
               <div className="p-2 flex items-center justify-between">
--- components/ui/loading-screen.jsx
+++ components/ui/loading-screen.jsx
@@ -1,13 +1,14 @@
-import { useEffect } from "react";
-import { TimelineMax, TweenMax } from "gsap";
-import ShearedRectangles from "./sheared-rectangles ";
-
 const LoadingScreen = () => {
- 
   return (
-    <div className="bg-black w-screen h-screen fixed top-0 left-0 z-[99]">
-      <ShearedRectangles />
-    </div>
+    // <div className="w-screen h-screen fixed z-[99] ">
+      <div className="bg-black/80 w-screen h-screen fixed top-0 left-0 z-[99] grid place-items-center">
+        <div id="loader">
+          <div id="box"></div>
+          <div id="hill"></div>
+        </div>
+        <p id="loading_screen_text">Loading ...</p>
+      </div>
+    // </div>
   );
 };
 
