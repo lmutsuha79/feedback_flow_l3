3d8a8600dd6680e60ee9ff45b7d37f9c9e491972
Khelil Yasser Djamel Eddie <74467521+lamani79@users.noreply.github.com>
2023-05-01 13:18:53 +0100

    get the data from api backend

--- pages/api/get-reviews.js
+++ pages/api/get-reviews.js
@@ -14,11 +14,9 @@ async function getAllReviews(appId) {
 }
 
 export default async (req, res) => {
-  // const app_id = "com.northcube.sleepcycle";
   const { gplay_id } = req.body;
-  console.log("the sent gpaly is ", gplay_id);
-  // const reviews = await getAllReviews(app_id);
-  // console.log(reviews.data.length);
-  // res.status(200).json({ ...reviews.data });
-  res.status(200).json({ gplay_id });
+  // console.log("the sent gpaly is ", gplay_id);
+  const reviews = await getAllReviews(gplay_id);
+  // console.log(reviews.data)
+  res.status(200).json({ reviews: reviews.data});
 };
--- pages/dashboard/reviews.js
+++ pages/dashboard/reviews.js
@@ -1,126 +1,139 @@
 import DashboardLayout from "@/components/layout/dashboard-layout";
 import { Table } from "flowbite-react";
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import { DashboardContext } from "../_app";
+import {
+  turnOffLoadinScreen,
+  turnOnLoadingScreen,
+} from "@/util/loadingFunctions";
 
 const Reviews = () => {
-  // async function getReviews(gplay_id) {
-  //   const res = await fetch("/api/get-reviews", {
-  //     method: "POST",
-  //     headers: { "Content-Type": "application/json" },
-  //     body: JSON.stringify({
-  //       gplay_id,
-  //     }),
-  //   });
-  //   const data = await res.json();
-  //   console.log(data);
-  // }
+  const { currentApp } = useContext(DashboardContext);
+  const [reviews, setReviews] = useState([]);
 
-  // const { currentApp } = useContext(DashboardContext);
+  async function getReviews() {
+    turnOnLoadingScreen();
+    const { gplay_id } = currentApp;
+    const res = await fetch("/api/get-reviews", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        gplay_id,
+      }),
+    });
+    const { reviews } = await res.json();
+    turnOffLoadinScreen();
 
-  // useEffect(() => {
-  //   if (currentApp) {
-  //     const { gplay_id } = currentApp;
-  //     getReviews(gplay_id);
-  //   }
-  // }, [currentApp]);
+    return reviews;
+  }
+  useEffect(() => {
+    if (currentApp) {
+      const reviews = getReviews().then((reviews) => setReviews(reviews));
+      // setReviews(reviews);
+    }
+  }, [currentApp]);
+
+  useEffect(() => {console.log(reviews)}, [reviews]);
+
   return (
     <DashboardLayout>
-      <main>
-        <Table striped={true}>
-          <Table.Head>
-            <Table.HeadCell>Product name</Table.HeadCell>
-            <Table.HeadCell>Color</Table.HeadCell>
-            <Table.HeadCell>Category</Table.HeadCell>
-            <Table.HeadCell>Price</Table.HeadCell>
-            <Table.HeadCell>
-              <span className="sr-only">Edit</span>
-            </Table.HeadCell>
-          </Table.Head>
-          <Table.Body className="divide-y">
-            <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
-              <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
-                Apple MacBook Pro 17"
-              </Table.Cell>
-              <Table.Cell>Sliver</Table.Cell>
-              <Table.Cell>Laptop</Table.Cell>
-              <Table.Cell>$2999</Table.Cell>
-              <Table.Cell>
-                <a
-                  href="/tables"
-                  className="font-medium text-blue-600 hover:underline dark:text-blue-500"
-                >
-                  Edit
-                </a>
-              </Table.Cell>
-            </Table.Row>
-            <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
-              <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
-                Microsoft Surface Pro
-              </Table.Cell>
-              <Table.Cell>White</Table.Cell>
-              <Table.Cell>Laptop PC</Table.Cell>
-              <Table.Cell>$1999</Table.Cell>
-              <Table.Cell>
-                <a
-                  href="/tables"
-                  className="font-medium text-blue-600 hover:underline dark:text-blue-500"
-                >
-                  Edit
-                </a>
-              </Table.Cell>
-            </Table.Row>
-            <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
-              <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
-                Magic Mouse 2
-              </Table.Cell>
-              <Table.Cell>Black</Table.Cell>
-              <Table.Cell>Accessories</Table.Cell>
-              <Table.Cell>$99</Table.Cell>
-              <Table.Cell>
-                <a
-                  href="/tables"
-                  className="font-medium text-blue-600 hover:underline dark:text-blue-500"
-                >
-                  Edit
-                </a>
-              </Table.Cell>
-            </Table.Row>
-            <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
-              <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
-                Google Pixel Phone
-              </Table.Cell>
-              <Table.Cell>Gray</Table.Cell>
-              <Table.Cell>Phone</Table.Cell>
-              <Table.Cell>$799</Table.Cell>
-              <Table.Cell>
-                <a
-                  href="/tables"
-                  className="font-medium text-blue-600 hover:underline dark:text-blue-500"
-                >
-                  Edit
-                </a>
-              </Table.Cell>
-            </Table.Row>
-            <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
-              <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
-                Apple Watch 5
-              </Table.Cell>
-              <Table.Cell>Red</Table.Cell>
-              <Table.Cell>Wearables</Table.Cell>
-              <Table.Cell>$999</Table.Cell>
-              <Table.Cell>
-                <a
-                  href="/tables"
-                  className="font-medium text-blue-600 hover:underline dark:text-blue-500"
-                >
-                  Edit
-                </a>
-              </Table.Cell>
-            </Table.Row>
-          </Table.Body>
-        </Table>
-      </main>
+      {reviews.length && (
+        <main>
+          <Table striped={true}>
+            <Table.Head>
+              <Table.HeadCell>Product name</Table.HeadCell>
+              <Table.HeadCell>Color</Table.HeadCell>
+              <Table.HeadCell>Category</Table.HeadCell>
+              <Table.HeadCell>Price</Table.HeadCell>
+              <Table.HeadCell>
+                <span className="sr-only">Edit</span>
+              </Table.HeadCell>
+            </Table.Head>
+            <Table.Body className="divide-y">
+              <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
+                <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
+                  Apple MacBook Pro 17"
+                </Table.Cell>
+                <Table.Cell>Sliver</Table.Cell>
+                <Table.Cell>Laptop</Table.Cell>
+                <Table.Cell>$2999</Table.Cell>
+                <Table.Cell>
+                  <a
+                    href="/tables"
+                    className="font-medium text-blue-600 hover:underline dark:text-blue-500"
+                  >
+                    Edit
+                  </a>
+                </Table.Cell>
+              </Table.Row>
+              <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
+                <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
+                  Microsoft Surface Pro
+                </Table.Cell>
+                <Table.Cell>White</Table.Cell>
+                <Table.Cell>Laptop PC</Table.Cell>
+                <Table.Cell>$1999</Table.Cell>
+                <Table.Cell>
+                  <a
+                    href="/tables"
+                    className="font-medium text-blue-600 hover:underline dark:text-blue-500"
+                  >
+                    Edit
+                  </a>
+                </Table.Cell>
+              </Table.Row>
+              <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
+                <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
+                  Magic Mouse 2
+                </Table.Cell>
+                <Table.Cell>Black</Table.Cell>
+                <Table.Cell>Accessories</Table.Cell>
+                <Table.Cell>$99</Table.Cell>
+                <Table.Cell>
+                  <a
+                    href="/tables"
+                    className="font-medium text-blue-600 hover:underline dark:text-blue-500"
+                  >
+                    Edit
+                  </a>
+                </Table.Cell>
+              </Table.Row>
+              <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
+                <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
+                  Google Pixel Phone
+                </Table.Cell>
+                <Table.Cell>Gray</Table.Cell>
+                <Table.Cell>Phone</Table.Cell>
+                <Table.Cell>$799</Table.Cell>
+                <Table.Cell>
+                  <a
+                    href="/tables"
+                    className="font-medium text-blue-600 hover:underline dark:text-blue-500"
+                  >
+                    Edit
+                  </a>
+                </Table.Cell>
+              </Table.Row>
+              <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
+                <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
+                  Apple Watch 5
+                </Table.Cell>
+                <Table.Cell>Red</Table.Cell>
+                <Table.Cell>Wearables</Table.Cell>
+                <Table.Cell>$999</Table.Cell>
+                <Table.Cell>
+                  <a
+                    href="/tables"
+                    className="font-medium text-blue-600 hover:underline dark:text-blue-500"
+                  >
+                    Edit
+                  </a>
+                </Table.Cell>
+              </Table.Row>
+            </Table.Body>
+          </Table>
+        </main>
+      )}
     </DashboardLayout>
   );
 };
